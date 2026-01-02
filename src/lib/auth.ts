88f5def3bd63f2b81f36a06e4457bf902c9f115d
noob-ubaid <ubaidurrahman661@gmail.com>
2026-01-03 00:15:41 +0600

    add some extra things

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -42,6 +42,7 @@ export const auth = betterAuth({
   },
   emailVerification: {
     sendOnSignUp: true,
+    autoSignInAfterVerification : true,
     sendVerificationEmail: async ({ user, url, token }) => {
       try {
         const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`;
@@ -117,7 +118,9 @@ If you did not create an account, you can safely ignore this email.
     },
   },
     socialProviders: {
-        google: { 
+        google: {
+            prompt : "select_account consent", 
+            accessType : "offline",
             clientId: process.env.GOOGLE_CLIENT_ID as string, 
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
         }, 
