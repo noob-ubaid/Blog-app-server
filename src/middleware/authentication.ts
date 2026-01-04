d4fbe86eb25c4dc6d9fd9e16a77e274901fc36a1
noob-ubaid <ubaidurrahman661@gmail.com>
2026-01-04 21:39:13 +0600

    add validation on middleware

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -3,10 +3,20 @@ import { auth } from "../lib/auth";
 
 export const authentication = (...roles: string[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const session = await auth.api.getSession({
-      headers : req.headers as any
-    })
-    console.log(session)
-    next()
+    try {
+      const session = await auth.api.getSession({
+        headers: req.headers as any,
+      });
+      if (!session) {
+        return res.status(401).json({
+          success: false,
+          message: "You are not authorized",
+        });
+      }
+      console.log(session);
+      next();
+    } catch (error: any) {
+      next(error);
+    }
   };
 };
